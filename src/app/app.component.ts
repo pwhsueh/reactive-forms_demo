ad218a3d63fa71f27258d39c769e045ba58b3d93
PWHsueh <pwhsueh@gmail.com>
2017-08-29 13:46:38 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div class="container">
+    <h1>Reactive Forms</h1>
+    <hero-detail></hero-detail>
+  </div>`
 })
 export class AppComponent {
   title = 'app works!';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
+import { HeroListComponent } from './hero-list.components';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
 
 import { AppComponent } from './app.component';
+// import { HeroDetailComponent1 } from './hero-detail.component';
+import { HeroDetailComponent } from './hero-detail.component'; // <-- #1 import
+import { HeroService } from './hero.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
+    HeroListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
